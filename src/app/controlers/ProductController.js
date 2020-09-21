6dca9abf61e59e8091d3edc9d6771694e0116128
sleduardo20 <sl.eduardo20@gmail.com>
2020-09-21 16:34:08 -0300

    chore: create form products

---
+++ src/app/controlers/ProductController.js
@@ -0,0 +1,5 @@
+module.exports ={
+  create(resquest, response){
+    return response.render('products/create.njk')
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 const express = require('express');
-
 const routes = express.Router();
+const ProductController = require('./app/controlers/ProductController')
 
-routes.get('/', (request, response) => response.render('layout.njk'));
+routes.get('/', (request, response) => 
+  response.render('layout.njk')
+  );
+
+routes.get('/products/create', ProductController.create);
+  
+
+routes.get('/ads/create', (request, response) => 
+  response.redirect('/products/create')
+);
 
 module.exports = routes;
